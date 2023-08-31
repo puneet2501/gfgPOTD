0bba638399739a19fa730579fabfcedb8cd468a8
Puneet <puneet19081@iiitd.ac.in>
2023-08-31 08:56:48 +0530

    java code AVL Tree Deletion

--- java/avl tree deletion.java
+++ java/avl tree deletion.java
@@ -200,86 +200,86 @@ class Node
 class Sol
 {
     Node root;
-    public static Node deleteNode(Node root, int key)
-    {
-        // code here.
-        if(root==null) return root;
-        if(key<root.data) root.left = deleteNode(root.left,key);
-        else if(key>root.data) root.right = deleteNode(root.right,key);
-        else{
-            if(root.left==null || root.right==null){
-                Node temp = null;
-                if(temp==root.left) temp = root.right;
-                else temp = root.left;
-                if(temp==null){
+    public static int height(Node root) {
+        if (root == null)
+            return 0;
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public static int getBalance(Node root) {
+        if (root == null)
+            return 0;
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return rightHeight - leftHeight;
+    }
+
+    public static Node leftRotation(Node x) {
+        Node y = x.right;
+        Node T = y.left;
+        x.right = T;
+        y.left = x;
+        return y;
+    }
+
+    public static Node rightRotation(Node x) {
+        Node y = x.left;
+        Node T = y.right;
+        x.left = T;
+        y.right = x;
+        return y;
+    }
+
+    public static int findMax(Node head) {
+        if (head == null)
+            return -1;
+        while (head.left != null) {
+            head = head.left;
+        }
+        return head.data;
+    }
+
+    public static Node deleteNode(Node root, int data) {
+        if (root == null)
+            return root;
+
+        if (root.data < data)
+            root.right = deleteNode(root.right, data);
+        else if (root.data > data)
+            root.left = deleteNode(root.left, data);
+        else {
+            if (root.left == null || root.right == null) {
+                Node temp = (root.left == null) ? root.right : root.left;
+                if (temp == null) {
                     temp = root;
                     root = null;
+                } else {
+                    root = temp;
                 }
-                else root = temp;
-            }
-            else{
-                Node temp = minValueNode(root.right);
-                root.data = temp.data;
-                root.right = deleteNode(root.right,temp.data);
+            } else {
+                int rep = findMax(root.right);
+                root.data = rep;
+                root.right = deleteNode(root.right, rep);
             }
         }
-        
-        if(root==null) return root;
-        root.height = Math.max(height(root.left),height(root.right)) + 1;
+
+        if (root == null)
+            return root;
+
         int balance = getBalance(root);
-        
-        if(balance>1 && getBalance(root.left)>=0) return rightRotate(root);
-        if(balance<-1 && getBalance(root.right)<=0) return leftRotate(root);
-        if(balance>1 && getBalance(root.left)<0){
-            root.left = leftRotate(root.left);
-            return rightRotate(root);
-        }
-        if(balance<-1 && getBalance(root.right)>0){
-            root.right = rightRotate(root.right);
-            return leftRotate(root);
+        if (balance > 1 && getBalance(root.right) >= 0)
+            return leftRotation(root);
+        else if (balance < -1 && getBalance(root.left) <= 0)
+            return rightRotation(root);
+        else if (balance > 1 && getBalance(root.right) < 0) {
+            root.right = rightRotation(root.right);
+            return leftRotation(root);
+        } else if (balance < -1 && getBalance(root.left) > 0) {
+            root.left = leftRotation(root.left);
+            return rightRotation(root);
         }
         return root;
     }
-    
-    static Node minValueNode(Node node){
-        Node curr = node;
-        while(curr.left!=null){
-            curr = curr.left;
-        }
-        return curr;
-    }
-    
-    static int getBalance(Node node){
-        if(node==null) return 0;
-        return height(node.left)-height(node.right);
-    }
-    static int height(Node node){
-        if(node==null) return 0;
-        return node.height;
-    }
-    static Node rightRotate(Node node){
-        Node l = node.left;
-        Node lr = l.right;
-        
-        l.right = node;
-        node.left = lr;
-        node.height = Math.max(height(node.left),height(node.right))+1;
-        l.height = Math.max(height(l.left),height(l.right))+1;
-        return l;
-    }
-    static Node leftRotate(Node node){
-        Node r = node.right;
-        Node rl = r.left;
-        
-        r.left = node;
-        node.right = rl;
-        
-        node.height = Math.max(height(node.left),height(node.right))+1;
-        r.height = Math.max(height(r.left),height(r.right)) + 1;
-        
-        return r;
-    }
 }
-
-
-
