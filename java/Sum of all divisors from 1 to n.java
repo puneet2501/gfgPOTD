0623057bbda36e3ad9875ff9bf7f531f53a658ba
Pankaj Kumar Bind <73558583+Pankaj-Bind@users.noreply.github.com>
2023-10-21 16:31:20 +0530

    commented in 3rd line

--- java/Sum of all divisors from 1 to n.java
+++ java/Sum of all divisors from 1 to n.java
@@ -1,6 +1,6 @@
 class Solution{
     static long sumOfDivisors(int N){
-         code here
+        //  code here
         long sum = 0;
         for(int i=1; i=N; i++) sum = sum + i(Ni);
         return sum;
