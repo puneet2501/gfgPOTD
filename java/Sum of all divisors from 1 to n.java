37ee0395af2eaf9a89815f9c3af609b801478b93
Pankaj Kumar Bind <73558583+Pankaj-Bind@users.noreply.github.com>
2023-10-21 16:32:36 +0530

    updated for loop logic

--- java/Sum of all divisors from 1 to n.java
+++ java/Sum of all divisors from 1 to n.java
@@ -2,7 +2,8 @@ class Solution{
     static long sumOfDivisors(int N){
         //  code here
         long sum = 0;
-        for(int i=1; i=N; i++) sum = sum + i(Ni);
+        for(int i=1; i<=N; i++) 
+            sum = sum + i(Ni);
         return sum;
     }
 }
