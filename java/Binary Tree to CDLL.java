7c157744b2a89e95c92fb477869f151b958a69e8
Puneet <puneet19081@iiitd.ac.in>
2023-11-17 21:32:29 +0530

    Binary Tree to CDLL

---
+++ java/Binary Tree to CDLL.java
@@ -0,0 +1,54 @@
+//User function Template for Java
+
+
+
+//User function Template for Java
+/*
+Node defined as
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}
+*/
+class Solution
+{ 
+    //Function to convert binary tree into circular doubly linked list.
+    Node bTreeToClist(Node root)
+    {
+        //your code here
+        Stack<Node> st = new Stack<>();
+        addToStack(root,st);
+        Node head,cur;
+        Node temp = st.pop();
+        addToStack(temp.right,st);
+        head = temp;
+        cur = head;
+        while(!st.isEmpty()){
+            //System.out.println(st);
+            temp = st.pop();
+            addToStack(temp.right,st);
+            cur.right = temp;
+            temp.left =cur;
+            cur=cur.right;
+            
+        }
+        cur.right=head;
+        head.left=cur;
+        return head;
+    }
+    
+      static void addToStack(Node root, Stack<Node> st){
+        if(root==null)return;
+        st.add(root);
+        while(root.left!=null){
+            st.add(root.left);
+            root=root.left;
+        }
+    }
+    
+}
+    
