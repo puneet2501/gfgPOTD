e04ed27e1bbd88358c6dc62690ebd35b1a246973
Puneet <85825588+puneet2501@users.noreply.github.com>
2024-01-08 16:19:39 +0530

    Update Merge 2 sorted linked list in reverse order.java

--- java/Merge 2 sorted linked list in reverse order.java
+++ java/Merge 2 sorted linked list in reverse order.java
@@ -48,3 +48,52 @@ class GfG
         return prv;
     }
 }
+
+
+
+// approach 2 tc : O(n+m) and sc : O(1)
+
+// class GfG
+// {
+//     Node mergeResult(Node node1, Node node2){
+//         Node n1 = reverse(node1);
+//         Node n2 = reverse(node2);
+        
+//         Node temp = new Node(0);
+//         Node res = temp;
+        
+//         while(true){
+//             if(n1==null) {
+//                 res.next=n2;
+//                 break;
+//             }
+//             if(n2==null){
+//                 res.next=n1;
+//                 break;
+//             }
+//             if(n1.data>n2.data){
+//                 res.next=n1;
+//                 n1 = n1.next;
+//             }
+//             else{
+//                 res.next=n2;
+//                 n2=n2.next;
+//             }
+//             res = res.next;
+            
+//         }
+//         return temp.next;
+//     }
+    
+//     Node reverse(Node a){
+//         Node prv = null,cur = a;
+//         while(cur!=null){
+//             Node temp = cur.next;
+//             cur.next = prv;
+//             prv = cur;
+//             cur = temp;
+//         }
+//         return prv;
+//     }
+// }
+
