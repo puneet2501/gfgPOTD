0bd32ee8897e5a7496d2d0191e2836b80e0c0e04
Puneet <85825588+puneet2501@users.noreply.github.com>
2024-01-09 15:22:19 +0530

    Update Merge 2 sorted linked list in reverse order.java

--- java/Merge 2 sorted linked list in reverse order.java
+++ java/Merge 2 sorted linked list in reverse order.java
@@ -11,89 +11,91 @@
 	}
 } */
 
-class GfG
-{
-    Node mergeResult(Node node1, Node node2){
-        Node n1 = reverse(node1);
-        Node n2 = reverse(node2);
-        
-        return mergeRecur(n1,n2);
-        
-    }
-    Node mergeRecur(Node A, Node B){
- 
-        if (A == null) return B;
-        if (B == null) return A;
- 
-        if (A.data > B.data) {
-            A.next = mergeRecur(A.next, B);
-            return A;
-        }
-        else {
-            B.next = mergeRecur(A, B.next);
-            return B;
-        }
-    }
-    
-    Node reverse(Node node){
-        Node prv = null;
-        Node cur = node;
-        
-        while(cur!=null){
-            Node next = cur.next;
-            cur.next = prv;
-            prv = cur;
-            cur = next;
-        }
-        return prv;
-    }
-}
 
-
-
-// approach 2 tc : O(n+m) and sc : O(1)
-
+//approach 1 using recursion 
 // class GfG
 // {
 //     Node mergeResult(Node node1, Node node2){
 //         Node n1 = reverse(node1);
 //         Node n2 = reverse(node2);
         
-//         Node temp = new Node(0);
-//         Node res = temp;
+//         return mergeRecur(n1,n2);
         
-//         while(true){
-//             if(n1==null) {
-//                 res.next=n2;
-//                 break;
-//             }
-//             if(n2==null){
-//                 res.next=n1;
-//                 break;
-//             }
-//             if(n1.data>n2.data){
-//                 res.next=n1;
-//                 n1 = n1.next;
-//             }
-//             else{
-//                 res.next=n2;
-//                 n2=n2.next;
-//             }
-//             res = res.next;
-            
+//     }
+//     Node mergeRecur(Node A, Node B){
+ 
+//         if (A == null) return B;
+//         if (B == null) return A;
+ 
+//         if (A.data > B.data) {
+//             A.next = mergeRecur(A.next, B);
+//             return A;
 //         }
-//         return temp.next;
+//         else {
+//             B.next = mergeRecur(A, B.next);
+//             return B;
+//         }
 //     }
     
-//     Node reverse(Node a){
-//         Node prv = null,cur = a;
+//     Node reverse(Node node){
+//         Node prv = null;
+//         Node cur = node;
+        
 //         while(cur!=null){
-//             Node temp = cur.next;
+//             Node next = cur.next;
 //             cur.next = prv;
 //             prv = cur;
-//             cur = temp;
+//             cur = next;
 //         }
 //         return prv;
 //     }
 // }
+
+
+
+// approach 2 tc : O(n+m) and sc : O(1)
+
+class GfG
+{
+    Node mergeResult(Node node1, Node node2){
+        Node n1 = reverse(node1);
+        Node n2 = reverse(node2);
+        
+        Node temp = new Node(0);
+        Node res = temp;
+        
+        while(true){
+            if(n1==null) {
+                res.next=n2;
+                break;
+            }
+            if(n2==null){
+                res.next=n1;
+                break;
+            }
+            if(n1.data>n2.data){
+                res.next=n1;
+                n1 = n1.next;
+            }
+            else{
+                res.next=n2;
+                n2=n2.next;
+            }
+            res = res.next;
+            
+        }
+        return temp.next;
+    }
+    
+    Node reverse(Node a){
+        Node prv = null,cur = a;
+        while(cur!=null){
+            Node temp = cur.next;
+            cur.next = prv;
+            prv = cur;
+            cur = temp;
+        }
+        return prv;
+    }
+}
 
